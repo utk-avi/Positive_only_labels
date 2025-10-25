eaf54ecdb19fce677fed3942b8a3ae09fb6a1a93
Aviral <aviralutkarsh87@gmail.com>
2025-10-25 13:37:59 +0530

    Second commit

--- log_regress.py
+++ log_regress.py
@@ -7,7 +7,8 @@ x_train, y_train = util.load_dataset('ds3_train.csv', add_intercept=True)
 _, t_train = util.load_dataset('ds3_train.csv', label_col='t')
 x_test, y_test = util.load_dataset('ds3_test.csv', add_intercept=True)
 _, t_test = util.load_dataset('ds3_test.csv', label_col='t')
-
+x_valid, y_valid = util.load_dataset('ds3_valid.csv', add_intercept=True)
+_, t_valid = util.load_dataset('ds3_valid.csv', label_col='t')
 
 
 class LogisticRegression(LinearModel):
@@ -25,13 +26,17 @@ class LogisticRegression(LinearModel):
             return -1 / m * np.dot(x.T, (y - h(theta, x)))
 
         def hessian(theta, x):
-            
+            """Vectorized implementation of the Hessian of J(theta).
+
+            :param theta: Shape (n,).
+            :param x:     All training examples of shape (m, n).
+            :return:      The Hessian of shape (n, n).
+            """
             m, _ = x.shape
             h_theta_x = np.reshape(h(theta, x), (-1, 1))
             return 1 / m * np.dot(x.T, h_theta_x * (1 - h_theta_x) * x)
 
         def next_theta(theta, x, y):
-            
             return theta - np.dot(np.linalg.inv(hessian(theta, x)), gradient(theta, x, y))
 
         m, n = x.shape
@@ -56,10 +61,34 @@ class LogisticRegression(LinearModel):
 log_reg = LogisticRegression()
 log_reg.fit(x_train, t_train)
 
-util.plot(x_train, t_train, log_reg.theta)
+util.plot(x_test, t_test, log_reg.theta)
 
-print("Theta is: ", log_reg.theta)
-print("The accuracy on training set is: ", np.mean(log_reg.predict(x_train) == t_train))
+print("The accuracy on test set for t is: ", np.mean(log_reg.predict(x_test) == t_test))
+
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+util.plot(x_test, y_test, log_reg.theta)
+print("The accuracy on test set for Y is: ", np.mean(log_reg.predict(x_test) == y_test))
+
+#Evaluate alpha
+
+def h(theta, x):
+    return 1 / (1 + np.exp(-np.dot(x, theta)))
+v_plus = x_valid[y_valid == 1]
+alpha = h(log_reg.theta, v_plus).mean()
+
+#Threshold condition
+def predict(theta, x):
+    return h(theta, x) / alpha >= 0.5
+
+theta_prime = log_reg.theta + np.log(2 / alpha - 1) * np.array([1, 0, 0])
+
+util.plot(x_test, y_test, theta_prime)
+print("The accuracy on valid set for y is: ", np.mean(log_reg.predict(x_test) == y_test))
+
+
+
 
 plt.show()
 
